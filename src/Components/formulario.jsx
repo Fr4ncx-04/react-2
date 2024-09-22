78b26962b97f4482cddd9e3785cd4950fd7b00f1
Fr4ncx-04 <145531469+Fr4ncx-04@users.noreply.github.com>
2024-09-21 21:01:55 -0600

    Update formulario.jsx

--- src/Components/formulario.jsx
+++ src/Components/formulario.jsx
@@ -1,43 +1,72 @@
 import React from 'react';
-import './formulario.css';
 
 const Formulario = () => {
-    return (
-      <form action="">
-        <div className="col-md-5 mt-2">
-          {/* Puedes agregar más campos aquí si es necesario */}
-        </div>
-        <div className="card">
-          <div className="card-header">Formulario</div>
-          <div className="card-body">
+  return (
+    <div className="col-md-5 mt-2">
+      <div className="card">
+        <div className="card-header">Formulario</div>
+        <div className="card-body">
+          <form action="">
             <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">Num. Ine:</span>
-              <input type="number" className="form-control" />
+              <span className="input-group-text" id="basic-addon1">Num. Matrícula:</span>
+              <input 
+                type="number" 
+                className="form-control" 
+                placeholder="Ingresa tu matrícula" 
+                aria-label="Número de matrícula" 
+              />
             </div>
+
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">Nombre:</span>
-              <input type="text" className="form-control" />
+              <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Ingresa tu nombre" 
+                aria-label="Nombre"
+              />
             </div>
+
             <div className="input-group mb-3">
-              <span className="input-group-text" id="basic-addon1">Apellido Paterno:</span>
-              <input type="text" className="form-control" />
+              <span className="input-group-text" id="basic-addon1">Apellido:</span>
+              <input 
+                type="text" 
+                className="form-control" 
+                placeholder="Ingresa tu apellido" 
+                aria-label="Apellido"
+              />
             </div>
+
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">Teléfono:</span>
-              <input type="tel" className="form-control" />
+              <input 
+                type="tel" 
+                className="form-control" 
+                placeholder="Ingresa tu teléfono" 
+                aria-label="Teléfono" 
+              />
             </div>
+
             <div className="input-group mb-3">
               <span className="input-group-text" id="basic-addon1">Correo:</span>
-              <input type="email" className="form-control" />
+              <input 
+                type="email" 
+                className="form-control" 
+                placeholder="Ingresa tu correo" 
+                aria-label="Correo" 
+              />
             </div>
-            <button type="submit" className="btn btn-success">Enviar</button>
-            
-          </div>
-          <button type="reset" className="btn btn-primary">Restablecer</button>
+
+            <div>
+              <button type="submit" className="btn btn-success">Enviar</button>
+            </div>
+
+          </form>
+          <button type="reset" className="btn btn-primary mt-2">Restablecer</button>
         </div>
-        
-      </form>
-    );
-  }
-  
-  export default Formulario;
+      </div>
+    </div>
+  );
+}
+
+export default Formulario;
